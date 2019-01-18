5319f3591072679ef3ef5b2d2042ed32ff7d37d8
Alexander Sudarchikov <35599728+asudarchikov@users.noreply.github.com>
2019-01-18 16:30:18 +0400

    Delete build-docker-image.sh

--- build-docker-image.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-echo “Building TfsToDocker image
-docker build -t adeptik/tfstodocker .
-docker login –username=”$1″ –password=”$2″
-echo “Pushing image…”
-docker push adeptik/tfstodocker
-docker logout
-exit 0
